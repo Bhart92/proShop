aafd4a383814aa549afcef877c263439687b3f72
Brandon <ihartappraisals@gmail.com>
2023-05-27 22:50:01 -0700

    setup product ui using dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { Container } from "react-bootstrap";
 const App = () => {
   return (
     <>
-      <h1>Welcome to ProShop</h1>
+      <Router>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:id" element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </>
   );
 };
